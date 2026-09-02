4e30178f9c6adc058a5ab66299f9100adda3e5ca
Devin <devinjaworek@gmail.com>
2026-09-02 12:43:07 +0200

    added some entities

---
+++ DnpAssignment1/Server/Entities/Forum.cs
@@ -0,0 +1,7 @@
+namespace Entities;
+
+public class Forum
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
--- DnpAssignment1/Server/Entities/Post.cs
+++ DnpAssignment1/Server/Entities/Post.cs
@@ -1,6 +1,11 @@
-namespace DefaultNamespace;
+namespace Entities;
 
 public class Post
 {
-    
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int Id { get; set; }
+    public int Likes { get; set; }
+    public int Dislikes { get; set; }
+    public DateTime Created { get; set; }
 }
